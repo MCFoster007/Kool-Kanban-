ccef629c56785cefeedd921b3ed0c98b51204a64
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-20 17:42:01 -0500

    connect to postgresql

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -15,6 +15,23 @@ const sequelize = process.env.DB_URL
       },
     });
 
+// let sequelize;
+
+// if (process.env.DB_URL) {
+//   sequelize = new Sequelize(process.env.DB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME || '', process.env.DB_USER ||'', process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//     },
+  
+//   );
+// }
+
+
+
+
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
